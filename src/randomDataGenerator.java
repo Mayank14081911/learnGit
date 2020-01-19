b53f671ef49be209fe39a47410f0a21fcf55b450
Mayank <stupid11mak@gmail.com>
2020-01-19 15:06:09 +0530

    second commit....

---
+++ src/randomDataGenerator.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Random;
+
+public class randomDataGenerator {
+
+	public static void main(String[] args) {
+		
+	Random r = new Random();
+	
+	System.out.println("random  number is "+ r.nextInt());
+	System.out.println("random  number is "+ r.nextFloat());
+	System.out.println("random  number is "+ r.toString());
+	System.out.println("random  number is "+ r.getClass());
+	
+	String email = "email"+r.nextInt()+"@gmail.com";
+	System.out.println(email);
+	}
+
+}
